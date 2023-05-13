0714c33096017f106d7c66aea6210e83c41f076d
Fiedor_ <stachfie@wp.pl>
2023-05-13 18:40:10 +0200

    php i poprawki do css

--- airport.php
+++ airport.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="pl">
 
 <head>
@@ -27,7 +28,16 @@
                 <th>status</th>
             </tr>
             <?php
-            //skrypt1
+            $conn = mysqli_connect('localhost', 'root', '', 'egzamin');
+            $q1 = mysqli_query($conn, "SELECT id,nr_rejsu,czas,kierunek,status_lotu FROM odloty ORDER BY czas DESC;");
+            foreach ($q1 as $row) {
+                echo "<tr>";
+                foreach ($row as $r) {
+                    echo "<td>" . $r . "</td>";
+                }
+                echo "</tr>";
+            }
+            mysqli_close($conn);
             ?>
         </table>
     </main>
@@ -37,7 +47,16 @@
         </div>
         <div id="e">
             <?php
-            //skrypt2
+            if (isset($_COOKIE['ciastko'])) {
+                echo "<p><b>Miło nam, że nas znowu odwiedziłeś</b></p>";
+            } else {
+                echo "<p><b>Dzień dobry! Sprawdź regulamin naszej strony</b></p>";
+            }
+            ?>
+            <?php
+            $name = "ciastko";
+            $value = "1";
+            setcookie($name, $value, time() + 3600);
             ?>
         </div>
         <div id="c">
