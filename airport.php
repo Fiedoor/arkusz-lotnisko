a5be1ba9c05a8ddef10ce2c0818be2098c737c3c
Fiedor_ <stachfie@wp.pl>
2023-05-13 17:52:16 +0200

    HTML i CSS

---
+++ airport.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Odloty samolotów</title>
+    <link rel="stylesheet" href="styl6.css">
+</head>
+
+<body>
+    <header>
+        <div id="a">
+            <h2>Odloty z lotniska</h2>
+        </div>
+        <div id='b'>
+            <img src="zad6.png" alt="logotyp">
+        </div>
+    </header>
+    <main>
+        <h4>tabela odlotów</h4>
+        <table>
+            <tr>
+                <th>Lp.</th>
+                <th>numer rejsu</th>
+                <th>czas</th>
+                <th>kierunek</th>
+                <th>status</th>
+            </tr>
+            <?php
+            //skrypt1
+            ?>
+        </table>
+    </main>
+    <footer>
+        <div id="c">
+            <a href="zrzuty/kw1.png" target="_blank">Pobierz obraz</a>
+        </div>
+        <div id="e">
+            <?php
+            //skrypt2
+            ?>
+        </div>
+        <div id="c">
+            Autor: Stanisław Fiedoruk 4J
+        </div>
+    </footer>
+</body>
+
+</html>
